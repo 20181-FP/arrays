1646b601f9d31d1fa6122b26be684e2baf1f0538
AngelDX <angel.condori@gmail.com>
2018-06-21 18:31:40 -0500

    tarea desarrollar la func delete

--- Vec04.java
+++ Vec04.java
@@ -2,18 +2,24 @@ public class Vec04{ //CRUD
 	public static void main(String[] args) {
 		String h[] = {"Hola", "Juan", "Maria"};
 		reader(h);
+
 		h=add("Angel", h);
 		reader(h);
-		//
-		int pos =find("Juan", h);
-		if(pos>=0)
-			System.out.println("pos="+pos);
-		else
-			System.out.println("No existe");
+		
 		edit("Juan","Juan Gabriel", h);
 		reader(h);
+
+		//delete("Juan Gabriel", h);
+		//reader(h);	
 	}
-	
+	public static void edit(String b,String n, String v[]){
+			int pos =find(b, v);
+			if(pos>=0)
+				v[pos]=n;
+			else
+				System.out.println("No existe");		
+			
+	}
 	public static int find(String b, String v[]){
 			for (int i=0;i<v.length;i++ ) {
 				if (b.equals( v[i]) ){	
@@ -21,7 +27,6 @@ public class Vec04{ //CRUD
 				}
 			}
 			return -1;
-		
 	}
 
 	public static String[] add(String cad,String v[]){
